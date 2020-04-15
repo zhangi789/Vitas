3c91b56f73736863c164f6c6c5d03ce7786a3e50
zhangi789 <www.392032143@qq.com>
2020-04-15 15:18:32 +0800

    兼容androidx

--- app/src/main/java/com/vitas/cn/MainActivity.java
+++ app/src/main/java/com/vitas/cn/MainActivity.java
@@ -1,10 +1,10 @@
 package com.vitas.cn;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
